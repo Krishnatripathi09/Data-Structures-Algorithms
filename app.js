5b74e4acc5033274424123fcc08038354d45721f
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-06-03 16:14:49 +0530

    doc(Patterns): wrote program to print various star patterns and added nodemon to automatically run the program on any changes in File

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-function smallest(arr) {
-  let smallest = Infinity;
+// function smallest(arr) {
+//   let smallest = Infinity;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < smallest) {
-      smallest = arr[i];
-    }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < smallest) {
+//       smallest = arr[i];
+//     }
+//   }
+//   return smallest;
+// }
+// console.log(smallest([12, 13, 100, 45, 67]));
+
+// for (let i = 0; i < 4; i++) {
+//   let row = " ";
+//   for (let j = 0; j <= i; j++) {
+//     row += "*";
+//   }
+//   console.log(row);
+// }
+
+// for (let i = 1; i < 5; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += j;
+//   }
+//   console.log(row);
+// }
+
+for (let i = 1; i <= 5; i++) {
+  let row = " ";
+  for (let j = 1; j <= 5 - i; j++) {
+    row += "*";
   }
-  return smallest;
+  console.log(row);
 }
-console.log(smallest([12, 13, 100, 45, 67]));
